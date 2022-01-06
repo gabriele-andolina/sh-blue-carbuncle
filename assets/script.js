f7bb3a633df114024ef2326b485a61a8205075c8
Gabriele Andolina <gabe.andolina@gmail.com>
2022-01-06 22:22:33 +0000

    Add JS interaction: game-afoot button function and scroll to next section

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,23 @@
 /* Button variables*/
 let mainButton = document.getElementById("main-button");
+let gameAfootButton = document.getElementById("game-afoot-button");
 
 /* Story parts' variables with their initial hidden status*/
 let instructions = document.getElementById("instructions");
 instructions.hidden = true;
+
+let partOne = document.getElementById("part-one");
+partOne.hidden = true;
 
 /* Button functions, show the next portion of the story*/
 mainButton.onclick = function() {
     instructions.hidden = false;
     mainButton.hidden = true;
     instructions.scrollIntoView({behavior: "smooth"});
+};
+
+gameAfootButton.onclick = function() {
+    partOne.hidden = false;
+    gameAfootButton.hidden = true;
+    partOne.scrollIntoView({behavior: "smooth"});
 };
